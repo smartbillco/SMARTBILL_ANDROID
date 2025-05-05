2ab10d27243ee1e17d1b88d693b8aedbf1e4b5db
AnnManjarres <ann.manjarres91@gmail.com>
2025-05-05 09:47:09 -0500

    Migration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smartbill/screens/cryptocurrencies/crypto_currency.dart';
 import 'package:smartbill/screens/splash/splash.dart';
 import 'package:smartbill/services/auth.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
@@ -13,7 +11,6 @@ import './route_observer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Initialize timezone data
   await FlutterDownloader.initialize();
   await Firebase.initializeApp();
   runApp(
@@ -31,14 +28,10 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      routes: {
-        '/cryptocurrency': (context) => const CryptoListScreen()
-      },
       navigatorObservers: <NavigatorObserver>[routeObserver],
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
